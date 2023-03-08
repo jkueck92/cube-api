000fc5cf3bb4494ea6932b8a1873214f03be2f71
jkueck <jkueck@eos-uptrade.de>
2023-03-08 13:07:41 +0100

    initial commit

---
+++ src/main/java/de/cube/cubeapi/CubeApiApplication.java
@@ -0,0 +1,13 @@
+package de.cube.cubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/cube/cubeapi/CubeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cube.cubeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
